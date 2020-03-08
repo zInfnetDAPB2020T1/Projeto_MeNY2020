3dffa770ef8fdc3f351d7d019092c1dca4b3c2a5
NicholasGCamargo <nicholascamargo@gmail.com>
2020-03-08 12:38:48 -0300

    First Commit

---
+++ app/src/main/java/com/example/projeto_meny2020/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.projeto_meny2020
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
