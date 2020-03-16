b94d3e22979f0ca573039fe80922db8ac94b2976
maedu <maria.aoliveira@al.infnet.edu.br>
2020-03-16 16:15:48 -0300

    Teste de bottomNavigation, tela de carregamento

---
+++ app/src/main/java/com/example/projeto_meny2020/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.projeto_meny2020.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
